5946f2ecb1137a2522e6252c7ef033153822cf56
draganzlatkovski <dragan_natalia@yahoo.com>
2013-10-02 23:38:13 +0200

    Update formUploadCSV.php

--- formUploadCSV.php
+++ formUploadCSV.php
@@ -1,16 +1,19 @@
-﻿<html>
+<html>
 <head>
 <title>Delieptrov</title>
-	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	
 	<script type="text/javascript">
         $(document).ready(function () {
 			$('#div1').hide();//na pocetok sokrij gi site
 			$('#div2').hide();
+			$('#div3').hide();
+			
 								
         $('#id_radio1').click(function () {
 			$('#div1').css({"width":"auto", "height":"auto", "-webkit-border-radius":"20px", "border-radius":"20px", "background":"rgba(239,243,246,0.7)"});
 		    $('#div1').show('fast');//pri klik na radio button1 prviot se prikazuva ostanatite se sokrieni
 			$('#div2').hide('fast');
+			$('#div3').hide('fast');
 			
 		});
 	    
@@ -18,6 +21,15 @@
 			$('#div2').css({"width":"auto", "height":"auto", "-webkit-border-radius":"20px", "border-radius":"20px", "background":"rgba(239,243,246,0.7)"});
 			$('#div1').hide('fast');
 			$('#div2').show('fast');
+			$('#div3').hide('fast');
+			
+	    });
+		
+		$('#id_radio3').click(function () {
+			$('#div2').css({"width":"auto", "height":"auto", "-webkit-border-radius":"20px", "border-radius":"20px", "background":"rgba(239,243,246,0.7)"});
+			$('#div1').hide('fast');
+			$('#div2').hide('fast');
+			$('#div3').show('fast');
 			
 	    });
 					 
@@ -29,19 +41,19 @@
 <body>
 <!-- Za koristenje na formata vo centralen del da se izvadat kometarite samo od tagot centar-->
 	<!--<center>-->
-		<h2>Choise</h2>
-
+		<h2>Choise your type for upload</h2>
 		<table style="width: 50%">
 			<tr>
 				<td><input id="id_radio1" type="radio" name="name_radio1" value="value_radio1" />SDP</td>
 				<td><input id="id_radio2" type="radio" name="name_radio1" value="value_radio2" />RL</td>
-				
-			</tr>
+				<td><input id="id_radio3" type="radio" name="name_radio1" value="value_radio3" />HIDE</td>				
+			</tr>			
 			<tr>
 				<td colspan="5">
 					<div id="div1">
 						<form action="uploadSDP.php" method="post" enctype="multipart/form-data" name="cvs" id="uploadform">					
 							<div>
+							</br>
 								<p>
 								Upload SDP File</br>
 									<select name='typedb' id='wroselect'>
@@ -54,12 +66,18 @@
 										<option value='flood_sdp'>Flood</option>
 										<option value='recreation_sdp'>Recreation</option>
 									</select> 								
-								<input name='ufile' type='file' size='28' id='ufile1' />
+								<input name='ufile' type='file' size='28' id='ufile0' />
 								<input type='submit' value='uploadSDP' name='uploadSDP' />
 								<span id='status' style='display:none'>...</span>
 								<iframe id='target_iframe' name='target_iframe' src='' style='width:0;height:0;border:0px'>
 								</iframe>
 								</p>
+							</div>
+							</br>
+							<div>
+							<p>
+							Red1:<input type="text" name="text1">&nbsp;&nbsp;&nbsp;&nbsp Red2 <input type="text" name="text2">
+							</p>
 							</div>
 						</form>
 					</div>
@@ -78,7 +96,7 @@
 										<option value='flood_rl'>Flood</option>
 										<option value='recreation_rl'>Recreation</option>
 									</select> 
-								<input name='ufile' type='file' size='28' id='ufile1' />
+								<input name='ufile' type='file' size='28' id='ufile0' />
 								<input type='submit' value='uploadRL' name='uploadRL' />
 								<span id='status' style='display:none'>...</span>
 								<iframe id='target_iframe' name='target_iframe' src='' style='width:0;height:0;border:0px'>
@@ -87,13 +105,18 @@
 							</div>
 						</form>
 					</div>
-					
-				
+					<div id="div3">
+						<form action="uploadRL.php" method="post" enctype="multipart/form-data" name="cvs" id="uploadform">
+							<div>
+								<p>
+								It's already hidden!
+								</p>
+							</div>
+						</form>
+					</div>						
 				</td>
 			</tr>
-		</table>
-	    
-		
+		</table>		
 	<!--</center>-->
 </body>
 </html>
